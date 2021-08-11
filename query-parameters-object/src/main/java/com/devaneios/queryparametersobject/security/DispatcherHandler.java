fbab8b7ee01485e703cf2d6fd5e9f797ab039ba7
NatanLisboa <natanflisboa1@gmail.com>
2021-08-11 01:41:08 -0300

    Apagando classe desnecessária

--- query-parameters-object/src/main/java/com/devaneios/queryparametersobject/security/DispatcherHandler.java
+++
@@ -1,9 +0,0 @@
-package com.devaneios.queryparametersobject.security;
-
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-public class DispatcherHandler extends RequestMappingHandlerMapping {
-
-
-
-}
